c4e02fe732e9545449e66a89d1a68e88498f93c0
ndsanja <ndsanja@gmail.com>
2021-10-30 05:13:24 +0700

    ganti penamaan utility class colour system menjadi my-primay-dan-sebagainya

--- pages/index.js
+++ pages/index.js
@@ -1,177 +1,169 @@
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-full p-8 space-y-2 bg-white">
+    <div className="flex flex-col items-center justify-center w-full p-8 space-y-2 bg-white text-body-large text-my">
       <div className="flex flex-col space-y-2 items-center justify-center bg-gray-100 p-8">
-        <h1 className="text-2xl text-md-on">light</h1>
+        <h1 className="text-body-large text-my-on-primary-container">light</h1>
         <div className="flex items-center justify-center w-full space-x-2">
-          <div className="p-4 bg-md-primary-_ text-md-on-primary-_">
-            primary
-          </div>
-          <div className="p-4 bg-md-on-primary-_ text-md-primary-_">
-            on primary
-          </div>
-          <div className="p-4 bg-md-primary-container text-md-on-primary-container">
+          <div className="p-4 bg-my-primary text-my-on-primary">primary</div>
+          <div className="p-4 bg-my-on-primary text-my-primary">on primary</div>
+          <div className="p-4 bg-my-primary-container text-my-on-primary-container">
             primary container
           </div>
-          <div className="p-4 bg-md-on-primary-container text-md-primary-container">
+          <div className="p-4 bg-my-on-primary-container text-my-primary-container">
             on primary container
           </div>
         </div>
 
         <div className="flex items-center justify-center w-full space-x-2">
-          <div className="p-4 bg-md-secondary-_ text-md-on-secondary-_">
+          <div className="p-4 bg-my-secondary text-my-on-secondary">
             secondary
           </div>
-          <div className="p-4 bg-md-on-secondary-_ text-md-secondary-_">
+          <div className="p-4 bg-my-on-secondary text-my-secondary">
             on secondary
           </div>
-          <div className="p-4 bg-md-secondary-container text-md-on-secondary-container">
+          <div className="p-4 bg-my-secondary-container text-my-on-secondary-container">
             secondary container
           </div>
-          <div className="p-4 bg-md-on-secondary-container text-md-secondary-container">
+          <div className="p-4 bg-my-on-secondary-container text-my-secondary-container">
             on secondary container
           </div>
         </div>
 
         <div className="flex items-center justify-center w-full space-x-2">
-          <div className="p-4 bg-md-tertiary-_ text-md-on-tertiary-_">
-            tertiary
-          </div>
-          <div className="p-4 bg-md-on-tertiary-_ text-md-tertiary-_">
+          <div className="p-4 bg-my-tertiary text-my-on-tertiary">tertiary</div>
+          <div className="p-4 bg-my-on-tertiary text-my-tertiary">
             on tertiary
           </div>
-          <div className="p-4 bg-md-tertiary-container text-md-on-tertiary-container">
+          <div className="p-4 bg-my-tertiary-container text-my-on-tertiary-container">
             tertiary container
           </div>
-          <div className="p-4 bg-md-on-tertiary-container text-md-tertiary-container">
+          <div className="p-4 bg-my-on-tertiary-container text-my-tertiary-container">
             on tertiary container
           </div>
         </div>
 
         <div className="flex items-center justify-center w-full space-x-2">
-          <div className="p-4 bg-md-error-_ text-md-on-error-_">error</div>
-          <div className="p-4 bg-md-on-error-_ text-md-error-_">on error</div>
-          <div className="p-4 bg-md-error-container text-md-on-error-container">
+          <div className="p-4 bg-my-error text-my-on-error">error</div>
+          <div className="p-4 bg-my-on-error text-my-error">on error</div>
+          <div className="p-4 bg-my-error-container text-my-on-error-container">
             error container
           </div>
-          <div className="p-4 bg-md-on-error-container text-md-error-container">
+          <div className="p-4 bg-my-on-error-container text-my-error-container">
             on error container
           </div>
         </div>
 
         <div className="flex items-center justify-center w-full space-x-2">
-          <div className="p-4 bg-md-background text-md-on-background">
+          <div className="p-4 bg-my-background text-my-on-background">
             background
           </div>
-          <div className="p-4 bg-md-on-background text-md-background">
+          <div className="p-4 bg-my-on-background text-my-background">
             on background
           </div>
-          <div className="p-4 bg-md-surface-_ text-md-on-surfabg-surface-_">
+          <div className="p-4 bg-my-surface text-my-on-surfabg-surface">
             surface
           </div>
-          <div className="p-4 bg-md-on-surface-_ text-md-surface-_">
-            on surface
-          </div>
+          <div className="p-4 bg-my-on-surface text-my-surface">on surface</div>
         </div>
 
         <div className="flex items-center justify-center w-full space-x-2">
-          <div className="p-4 bg-md-surface-variant text-md-on-surface-variant">
+          <div className="p-4 bg-my-surface-variant text-my-on-surface-variant">
             surface variant
           </div>
-          <div className="p-4 bg-md-on-surface-variant text-md-surface-variant">
+          <div className="p-4 bg-my-on-surface-variant text-my-surface-variant">
             on surface variant
           </div>
-          <div className="p-4 bg-md-outline text-md-on-surface-outline">
+          <div className="p-4 bg-my-outline text-my-on-surface-outline">
             outline
           </div>
         </div>
       </div>
 
       <div className="flex flex-col space-y-2 items-center justify-center bg-black p-8">
-        <h1 className="text--2xl text-md-dark-on-primary-container">dark</h1>
+        <h1 className="text--2xl text-my-dark-on-primary-container">dark</h1>
         <div className="flex items-center justify-center w-full space-x-2">
-          <div className="p-4 bg-md-dark-primary-_ text-md-dark-on-primary-_">
+          <div className="p-4 bg-my-dark-primary text-my-dark-on-primary">
             primary
           </div>
-          <div className="p-4 bg-md-dark-on-primary-_ text-md-dark-primary-_">
+          <div className="p-4 bg-my-dark-on-primary text-my-dark-primary">
             on primary
           </div>
-          <div className="p-4 bg-md-dark-primary-container text-md-dark-on-primary-container">
+          <div className="p-4 bg-my-dark-primary-container text-my-dark-on-primary-container">
             primary container
           </div>
-          <div className="p-4 bg-md-dark-on-primary-container text-md-dark-primary-container">
+          <div className="p-4 bg-my-dark-on-primary-container text-my-dark-primary-container">
             on primary container
           </div>
         </div>
 
         <div className="flex items-center justify-center w-full space-x-2">
-          <div className="p-4 bg-md-dark-secondary-_ text-md-dark-on-secondary-_">
+          <div className="p-4 bg-my-dark-secondary text-my-dark-on-secondary">
             secondary
           </div>
-          <div className="p-4 bg-md-dark-on-secondary-_ text-md-dark-secondary-_">
+          <div className="p-4 bg-my-dark-on-secondary text-my-dark-secondary">
             on secondary
           </div>
-          <div className="p-4 bg-md-dark-secondary-container text-md-dark-on-secondary-container">
+          <div className="p-4 bg-my-dark-secondary-container text-my-dark-on-secondary-container">
             secondary container
           </div>
-          <div className="p-4 bg-md-dark-on-secondary-container text-md-dark-secondary-container">
+          <div className="p-4 bg-my-dark-on-secondary-container text-my-dark-secondary-container">
             on secondary container
           </div>
         </div>
 
         <div className="flex items-center justify-center w-full space-x-2">
-          <div className="p-4 bg-md-dark-tertiary-_ text-md-dark-on-tertiary-_">
+          <div className="p-4 bg-my-dark-tertiary text-my-dark-on-tertiary">
             tertiary
           </div>
-          <div className="p-4 bg-md-dark-on-tertiary-_ text-md-dark-tertiary-_">
+          <div className="p-4 bg-my-dark-on-tertiary text-my-dark-tertiary">
             on tertiary
           </div>
-          <div className="p-4 bg-md-dark-tertiary-container text-md-dark-on-tertiary-container">
+          <div className="p-4 bg-my-dark-tertiary-container text-my-dark-on-tertiary-container">
             tertiary container
           </div>
-          <div className="p-4 bg-md-dark-on-tertiary-container text-md-dark-tertiary-container">
+          <div className="p-4 bg-my-dark-on-tertiary-container text-my-dark-tertiary-container">
             on tertiary container
           </div>
         </div>
 
         <div className="flex items-center justify-center w-full space-x-2">
-          <div className="p-4 bg-md-dark-error-_ text-md-dark-on-error-_">
+          <div className="p-4 bg-my-dark-error text-my-dark-on-error">
             error
           </div>
-          <div className="p-4 bg-md-dark-on-error-_ text-md-dark-error-_">
+          <div className="p-4 bg-my-dark-on-error text-my-dark-error">
             on error
           </div>
-          <div className="p-4 bg-md-dark-error-container text-md-dark-on-error-container">
+          <div className="p-4 bg-my-dark-error-container text-my-dark-on-error-container">
             error container
           </div>
-          <div className="p-4 bg-md-dark-on-error-container text-md-dark-error-container">
+          <div className="p-4 bg-my-dark-on-error-container text-my-dark-error-container">
             on error container
           </div>
         </div>
 
         <div className="flex items-center justify-center w-full space-x-2">
-          <div className="p-4 bg-md-dark-background text-md-dark-on-background">
+          <div className="p-4 bg-my-dark-background text-my-dark-on-background">
             background
           </div>
-          <div className="p-4 bg-md-dark-on-background text-md-dark-background">
+          <div className="p-4 bg-my-dark-on-background text-my-dark-background">
             on background
           </div>
-          <div className="p-4 bg-md-dark-surface-_ text-md-dark-on-surface-_">
+          <div className="p-4 bg-my-dark-surface text-my-dark-on-surface">
             surface
           </div>
-          <div className="p-4 bg-md-dark-on-surface-_ text-md-dark-surface-_">
+          <div className="p-4 bg-my-dark-on-surface text-my-dark-surface">
             on surface
           </div>
         </div>
 
         <div className="flex items-center justify-center w-full space-x-2">
-          <div className="p-4 bg-md-dark-surface-variant text-md-dark-on-surface-variant">
+          <div className="p-4 bg-my-dark-surface-variant text-my-dark-on-surface-variant">
             surface variant
           </div>
-          <div className="p-4 bg-md-dark-on-surface-variant text-md-dark-surface-variant">
+          <div className="p-4 bg-my-dark-on-surface-variant text-my-dark-surface-variant">
             on surface variant
           </div>
-          <div className="p-4 bg-md-dark-outline text-md-dark-on-surface-outline">
+          <div className="p-4 bg-my-dark-outline text-my-dark-on-surface-outline">
             outline
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,108 +5,80 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        //LIGHT COLOURS
-        md: {
-          primary: {
-            _: "var(--primary)",
-            container: "var(--primary-container)",
-          },
-          secondary: {
-            _: "var(--secondary)",
-            container: "var(--secondary-container)",
-          },
-          tertiary: {
-            _: "var(--tertiary)",
-            container: "var(--tertiary-container)",
-          },
-          error: {
-            _: "var(--error)",
-            container: "var(--error-container)",
-          },
-          background: "var(--background)",
+        "my-primary": "var(--primary)",
+        "my-on-primary": "var(--on-primary)",
+        "my-primary-container": "var(--primary-container)",
+        "my-on-primary-container": "var(--on-primary-container)",
+        "my-dark-primary": "var(--primary-dark)",
+        "my-dark-on-primary": "var(--on-primary-dark)",
+        "my-dark-primary-container": "var(--primary-container-dark)",
+        "my-dark-on-primary-container": "var(--on-primary-container-dark)",
 
-          surface: {
-            _: "var(--surface)",
-            variant: "var(--surface-variant)",
-          },
-          outline: "var(--outline)",
+        "my-secondary": "var(--secondary)",
+        "my-on-secondary": "var(--on-secondary)",
+        "my-secondary-container": "var(--secondary-container)",
+        "my-on-secondary-container": "var(--on-secondary-container)",
+        "my-dark-secondary": "var(--secondary-dark)",
+        "my-dark-on-secondary": "var(--on-secondary-dark)",
+        "my-dark-secondary-container": "var(--secondary-container-dark)",
+        "my-dark-on-secondary-container": "var(--on-secondary-container-dark)",
 
-          on: {
-            primary: {
-              _: "var(--on-primary)",
-              container: "var(--on-primary-container)",
-            },
-            secondary: {
-              _: "var(--on-secondary)",
-              container: "var(--on-secondary-container)",
-            },
-            tertiary: {
-              _: "var(--on-tertiary)",
-              container: "var(--on-tertiary-container)",
-            },
-            error: {
-              _: "var(--on-error)",
-              container: "var(--on-error-container)",
-            },
-            background: "var(--on-background)",
-            surface: {
-              _: "var(--on-surface)",
-              variant: "var(--on-surface-variant)",
-            },
-          },
-          //DARK COLOURS
+        "my-tertiary": "var(--tertiary)",
+        "my-on-tertiary": "var(--on-tertiary)",
+        "my-tertiary-container": "var(--tertiary-container)",
+        "my-on-tertiary-container": "var(--on-tertiary-container)",
+        "my-dark-tertiary": "var(--tertiary-dark)",
+        "my-dark-on-tertiary": "var(--on-tertiary-dark)",
+        "my-dark-tertiary-container": "var(--tertiary-container-dark)",
+        "my-dark-on-tertiary-container": "var(--on-tertiary-container-dark)",
 
-          dark: {
-            primary: {
-              _: "var(--primary-dark)",
-              container: "var(--primary-container-dark)",
-            },
-            secondary: {
-              _: "var(--secondary-dark)",
-              container: "var(--secondary-container-dark)",
-            },
-            tertiary: {
-              _: "var(--tertiary-dark)",
-              container: "var(--tertiary-container-dark)",
-            },
-            error: {
-              _: "var(--error-dark)",
-              container: "var(--error-container-dark)",
-            },
-            background: "var(--background-dark)",
+        "my-error": "var(--error)",
+        "my-on-error": "var(--on-error)",
+        "my-error-container": "var(--error-container)",
+        "my-on-error-container": "var(--on-error-container)",
+        "my-dark-error": "var(--error-dark)",
+        "my-dark-on-error": "var(--on-error-dark)",
+        "my-dark-error-container": "var(--error-container-dark)",
+        "my-dark-on-error-container": "var(--on-error-container-dark)",
 
-            surface: {
-              _: "var(--surface-dark)",
-              variant: "var(--surface-variant-dark)",
-            },
-            outline: "var(--outline-dark)",
+        "my-background": "var(--background)",
+        "my-on-background": "var(--on-background)",
+        "my-dark-background": "var(--background-dark)",
+        "my-dark-on-background": "var(--on-background-dark)",
 
-            on: {
-              primary: {
-                _: "var(--on-primary-dark)",
-                container: "var(--on-primary-container-dark)",
-              },
-              secondary: {
-                _: "var(--on-secondary-dark)",
-                container: "var(--on-secondary-container-dark)",
-              },
-              tertiary: {
-                _: "var(--on-tertiary-dark)",
-                container: "var(--on-tertiary-container-dark)",
-              },
-              error: {
-                _: "var(--on-error-dark)",
-                container: "var(--on-error-container-dark)",
-              },
-              background: "var(--on-background-dark)",
+        "my-surface": "var(--surface)",
+        "my-on-surface": "var(--on-surface)",
+        "my-dark-surface": "var(--surface-dark)",
+        "my-dark-on-surface": "var(--on-surface-dark)",
 
-              surface: {
-                _: "var(--on-surface-dark)",
-                variant: "var(--on-surface-variant-dark)",
-              },
-            },
-          },
-        },
+        "my-surface-variant": "var(--surface-variant)",
+        "my-on-surface-variant": "var(--on-surface-variant)",
+        "my-dark-surface-variant": "var(--surface-variant-dark)",
+        "my-dark-on-surface-variant": "var(--on-surface-variant-dark)",
+
+        "my-outline": "var(--outline)",
+        "my-on-outline": "var(--on-outline)",
+      },
+      fontSize: {
+        "display-large": "var(--font-display-large)",
+        "display-medium": "var(--font-display-medium)",
+        "display-small": "var(--font-display-small)",
+
+        "headline-large": "var(--font-headline-large)",
+        "headline-medium": "var(--font-headline-medium)",
+        "headline-small": "var(--font-headline-small)",
+
+        "title-large": "var(--font-title-large)",
+        "title-medium": "var(--font-title-medium)",
+        "title-small": "var(--font-title-small)",
+
+        "label-large": "var(--font-label-large)",
+        "label-medium": "var(--font-label-medium)",
+        "label-small": "var(--font-label-small)",
+
+        "body-large": "var(--font-body-large)",
+        "body-medium": "var(--font-body-medium)",
+        "body-small": "var(--font-body-small)",
       },
     },
   },
