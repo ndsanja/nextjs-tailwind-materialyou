cdd4d4771a263a2319bd1b9b3effbfc627ecc45f
ndsanja <ndsanja@gmail.com>
2021-10-29 10:22:13 +0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="flex items-center justify-center w-full">
+      <h1 className="max-w-screen-2xl text-2xl text-[#ff6347]">Hello World</h1>
+    </div>
+  );
+}
+
+export default Home;
